a0ac80868fea76abc786a055e4d78b744915514e
Alex Buck <alexjbuck@gmail.com>
2021-12-03 21:18:40 -0500

    fix(pages): Resolve to use location.href prop
    location.href prop with appropriate checking will return the actual
    URL. siteMetadata query returns a stored value
    that may not be the correct value for the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,13 @@
 import * as React from "react"
 import Main from "../layouts/Main"
-import SiteMetadata from '../queries/SiteMetadata'
 
 
 const Index = ({location}) => {
 
   const url = location.href ? location.href : '';
-  const { siteUrl } = SiteMetadata()
   return (
     <Main>
-      <div>
-        {siteUrl}
-      </div>
-      <div>
-        {url}
-      </div>
+      {url}
     </Main>
   )
 }
