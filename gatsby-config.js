d1c7d3c87f43ae07571d0405c6f9c3e6ba2d85f2
Alex Buck <alexjbuck@gmail.com>
2021-12-03 22:06:32 -0500

    add(meta): Add siteUrl and author metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://myfitnesspaldashboardmain.gatsbyjs.io/",
     title: "My Fitness Pal Dashboard",
+    author: "Alex Buck",
   },
   plugins: [],
 };
